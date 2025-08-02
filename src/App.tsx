077c50f1f7bce5a513525fe72f10701d356d3c81
minsu0707 <mandoo4137@gmail.com>
2025-08-03 00:57:57 +0900

    yarn

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { theme, GlobalStyles } from "./themes";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
