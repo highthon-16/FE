76742ddac927bb6c1475bd0fc2b0792c2786e8d7
seungW0o <ssounsowns@gmail.com>
2025-08-03 00:38:28 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "@emotion/react";
 import { theme, GlobalStyles } from "./themes";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
